ebee3ec09332495532888e25524018f492525478
Kriskooo <kris.danielov@gmail.com>
2021-02-04 22:54:04 +0200

    ObjectAndClassesExercise

---
+++ Students/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace Students
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public double Grade { get; set; }
+        public Student(string firstName, string secondName, double grade)
+        {
+            this.FirstName = firstName;
+            this.SecondName = secondName;
+            this.Grade = grade;
+        }
+        public double getGrade()
+        {
+            return this.Grade;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(this.FirstName, this.SecondName, this.Grade);
+        }
+    }
+}
